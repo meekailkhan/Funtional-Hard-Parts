b248b3d934f9ca29244391c686e1e0b8c40042b0
pc <meekailkhan123aar@gmail.com>
2024-09-23 00:49:21 +0530

    functional hard pard javascript

--- gotchas.js
+++ gotchas.js
@@ -3,3 +3,44 @@
 // kuch common gotchas jo samjhne wali hai
 
 // 1.Type coercion (automatic type convertion)
+console.log(1 + "2") // "12"
+console.log(1 - "2"); // 2
+
+
+// 2.Variable hoisting
+console.log(myVar)  // undefinde
+var myVar = 10;
+
+
+// 3.This keyword
+const obj = {
+    name : "meekail",
+    getName : function() {
+        return this.name
+    }
+}
+
+const name1 = obj.getName();
+const name2 = obj.getName
+
+console.log(name1) // "meekail"
+console.log(name2()) // undefined
+
+
+// 4.comparing == and ===
+console.log(0 == "") // true
+console.log(0 === "") // false
+
+
+// 5.Closure Gotcha (looping with var)
+for(var i = 0 ; i < 3 ; i++){
+    setTimeout(()=>{
+        console.log(i)
+    },1000)  
+} // print three time 3
+
+for(let i = 0 ; i < 3 ; i++){
+    setTimeout(()=>{
+        console.log(i)
+    },1000)
+} // print 1,2 and 3 
