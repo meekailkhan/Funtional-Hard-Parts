50ea9783e08ff14f8e391e73471e9099438c82d1
pc <meekailkhan123aar@gmail.com>
2024-09-22 18:14:53 +0530

    functional hard javascript

---
+++ gotchas.js
@@ -0,0 +1,5 @@
+// JavaScript mein "gotchas" wo tricky situations ya unexpected behaviors hote hain jo programming karte waqt naya confusion ya bugs create kar sakte hain. Yeh wo points hain jo beginners ya kabhi-kabhi experienced developers ko bhi dikkat mein daal dete hain, 
+
+// kuch common gotchas jo samjhne wali hai
+
+// 1.Type coercion (automatic type convertion)
---
+++ immutibility.js
@@ -0,0 +1,5 @@
+let arr = [1,2,3,4];
+
+const multiplyBy2 = num => num * 2;
+
+let result = arr.map(multiplyBy2)
