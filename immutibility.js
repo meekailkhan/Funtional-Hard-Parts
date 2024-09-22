db3c05c89edaa118f33521ff600473d5ce900efe
pc <meekailkhan123aar@gmail.com>
2024-09-22 18:15:27 +0530

    functional hard javascript

--- immutibility.js
+++ immutibility.js
@@ -2,4 +2,10 @@ let arr = [1,2,3,4];
 
 const multiplyBy2 = num => num * 2;
 
-let result = arr.map(multiplyBy2)
+let result = arr.map(multiplyBy2);
+let newResult = arr.map(multiplyBy2);
+
+console.log(result); // [2,4,6,8]
+console.log(newResult); // [2,4,6,8];
+
+
